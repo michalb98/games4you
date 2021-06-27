38e63d68eb9c9ea61719814eb4535938d79d6b81
michalb98 <michalb98.michal.blaszczyk98@gmail.com>
2021-06-27 16:51:07 +0200

    27.06.21

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Games4You - sklep z grami komputerowymi</title>
+    <link rel="stylesheet" href="./css/main-style.css">
+</head>
+<body>
+    <div id="main-header">
+        <a href="strona-glowna" title="Przejdź do strony głównej">
+            <div id="logo">
+                <img src="./img/web/logo.png" alt="Games4You">
+            </div>
+        </a>
+    </div>
+</body>
+</html>
